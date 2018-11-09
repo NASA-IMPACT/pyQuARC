a552d816365db2a91aee518c720e32a7ed26ab13
pstatonvt <pstaton@vt.edu>
2018-11-09 10:21:48 -0600

    Checkpoint to create a branch.

--- dif_schema_builder.py
+++ dif_schema_builder.py
@@ -11,13 +11,18 @@ class DifSchemaBuilder:
 
     def dif_schema(variable_name):
         schema_output = []
+        type_output = []
+
         dif_schema_url = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster'
         response = requests.get(dif_schema_url)
         schema_xsd = BytesIO(response.content)
         xsd_file = etree.parse(schema_xsd)
+        
         for value in xsd_file.findall('*'):
             if value.get('name') == variable_name:
                 restrictions = value.findall('{http://www.w3.org/2001/XMLSchema}restriction')[0]
+                type_output.append(restrictions.get('base'))
                 for restriction in restrictions.findall('*'):
-                    schema_output.append(restriction.get('value'))                    
-        return schema_output
+                    schema_output.append(restriction.get('value'))
+
+        return schema_output, type_output
