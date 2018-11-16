579e6b25fa53118b70460b9aa4eaea1293f564a6
pstatonvt <pstaton@vt.edu>
2018-11-16 12:10:58 -0600

    Iksha edits checkpoint.

--- dif_schema_builder.py
+++ dif_schema_builder.py
@@ -4,25 +4,41 @@ DIF Schema Builder
 
 # Import necessary libraries
 import requests
+import json
 from io import BytesIO
 from lxml import etree
 
+# Shared global values
+SCHEMA_URL = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster'
+VARIABLE_LIST = ['GranuleSpatialRepresentationEnum', 'CoordinateSystemEnum',
+'OrganizationTypeEnum' ,'PersonnelRoleEnum','DatasetLanguageEnum',
+'SpatialCoverageTypeEnum', 'PhoneTypeEnum', 'DatasetProgressEnum',
+'PersistentIdentifierEnum']
+dif_dictionary = dict()
+
 class DifSchemaBuilder:
+    def __init__(self):
+        self.dif_dictionary = dict()
 
-    def dif_schema(variable_name):
-        schema_output = []
-        type_output = []
-
-        dif_schema_url = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster'
-        response = requests.get(dif_schema_url)
+    def parse_url(self):
+        response = requests.get(SCHEMA_URL)
         schema_xsd = BytesIO(response.content)
         xsd_file = etree.parse(schema_xsd)
-        
-        for value in xsd_file.findall('*'):
-            if value.get('name') == variable_name:
-                restrictions = value.findall('{http://www.w3.org/2001/XMLSchema}restriction')[0]
-                type_output.append(restrictions.get('base'))
-                for restriction in restrictions.findall('*'):
-                    schema_output.append(restriction.get('value'))
+        return xsd_file
 
-        return schema_output, type_output
+    def build_schema(self):
+        schema_output = []
+        type_output = []
+        # parse value function
+        for variable in VARIABLE_LIST:
+            for value in self.parse_url().findall('*'):
+                if value.get('name') == variable:
+                    restrictions = value.findall('{http://www.w3.org/2001/XMLSchema}restriction')[0]
+                    type_output.append(restrictions.get('base'))
+                    for restriction in restrictions.findall('*'):
+                        schema_output.append(restriction.get('value'))
+            self.dif_dictionary[variable] = schema_output
+
+    def save(self):
+        with open('dif_schema_output.json', 'w') as outfile:
+            json.dump(self.dif_dictionary, outfile)
--- workbench.py
+++ workbench.py
@@ -7,14 +7,6 @@ from dif_schema_builder import DifSchemaBuilder
 import json
 
 # Workspace
-dif_dictionary = dict()
-variable_list = ['GranuleSpatialRepresentationEnum', 'CoordinateSystemEnum',
-'OrganizationTypeEnum' ,'PersonnelRoleEnum','DatasetLanguageEnum',
-'SpatialCoverageTypeEnum', 'PhoneTypeEnum', 'DatasetProgressEnum',
-'PersistentIdentifierEnum']
-
-for variable in variable_list:
-    dif_dictionary[variable] = DifSchemaBuilder.dif_schema(variable)
-
-with open('dif_schema_output.json', 'w') as outfile:
-    json.dump(dif_dictionary, outfile)
+dsb = DifSchemaBuilder()
+dsb.build_schema()
+dsb.save()
