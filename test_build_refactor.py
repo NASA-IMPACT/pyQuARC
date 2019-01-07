8abd29d303fcbf7a4246075c1e31e9fa9da3fa5a
Carson Davis <CarsonReidDavis@gmail.com>
2019-01-07 13:30:07 -0600

    Completely restructured the program to search and find specific entries instead of the old structure of looping through the entire tree and extracting the desired entries as they were found. Removed console print function.

--- test_build_refactor.py
+++ test_build_refactor.py
@@ -2,12 +2,10 @@ from dif_schema_builder_refactor import DifSchemaBuilder
 
 # tests using default file
 test = DifSchemaBuilder()
-
-
 test.save_json()
+test.self_test()
 
 # test using user enterd file
-# test_2 = DifSchemaBuilder('https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster')
-# test_2.print_tree()
-# test_2.save_json()
-# test_2.self_test()
+test_2 = DifSchemaBuilder('https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster')
+test_2.save_json()
+test_2.self_test()
