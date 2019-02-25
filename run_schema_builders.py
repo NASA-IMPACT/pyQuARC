df480826a8a0ea1d9aa6a63307fdf492214a4f89
Carson Davis <CarsonReidDavis@gmail.com>
2019-02-25 17:08:21 -0600

    refactor schema_builder
    major refactor, new code should work on DIF and ECHO formats
    Finalized recursion system so that it works on top level simpleTypes and
    on the DIF formats. Changed benchmark DIF json. Removed now uncessessary
    class inheritance. Simplified some overlapping functions. Updated class
    names. Removed default website to generalize class.

--- run_schema_builders.py
+++ run_schema_builders.py
@@ -3,20 +3,13 @@ from schema_builders import *
 SCHEMA_URL_DIF = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd'
 SCHEMA_URL_ECHO = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/echo-schemas/raw/schemas/10.0/Collection.xsd'
 
-testEcho = EchoSchema(SCHEMA_URL_ECHO)
-# testDif.build_dict()
-# testEcho.save_json()
-print(testEcho.schema_dict)
-print()
-print(testEcho.schema_dict['CollectionMetaDataFile'])
-# testEcho = DifSchema(SCHEMA_URL_ECHO)
-# testEcho.print_xsd_structure()
-# testDif.save_json()
-
-
-
 #
-# testEcho = EchoSchema()
-# testEcho.print_xsd_structure()
+# testDif = SchemaBuilder(SCHEMA_URL_DIF)
+# testDif.print_xsd_structure()
+# testDif.save_json('test_DIF.json')
+
+testEcho = SchemaBuilder(SCHEMA_URL_ECHO)
+testEcho.print_xsd_structure()
+testEcho.save_json('test_ECHO.json')
 
 
--- schema_builders.py
+++ schema_builders.py
@@ -11,12 +11,10 @@ from io import BytesIO
 from lxml import etree
 
 # shared global values
-SCHEMA_URL_DIF = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd'
-SCHEMA_URL_ECHO = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/echo-schemas/raw/schemas/10.0/Collection.xsd'
 BASE_SCHEMA = '{http://www.w3.org/2001/XMLSchema}'
 
 
-class SchemaTools:
+class SchemaBuilder:
 
     def __init__(self, schema_url_input):
         '''
@@ -27,6 +25,9 @@ class SchemaTools:
         self.schema_dict = dict()
         self.schema_tree = None
         self.schema_url = schema_url_input
+
+        self.elem_functions = {'element': self._element,
+                               'simpleType': self._simple}
 
         self.xsd_import(self.schema_url)
 
@@ -42,54 +43,55 @@ class SchemaTools:
 
         self.schema_tree = etree.parse(schema_file)  # could fail for a lot of reasons
 
+        self.build_dict()
+
     def save_json(self, json_path='schema.json'):
         '''
         Rebuilds the dictionary (in case user has imported new url), and saves a json file.
         '''
 
-        self.build_dict()
-
         with open(json_path, 'w') as outfile:
             json.dump(self.schema_dict, outfile)
 
-    def print_xsd_structure(self):
-        for count, element in enumerate(self.schema_tree.findall('*')):
-            print(f'{count}: {self._extract_tag(element)}: {element.get("name")}')
-            # self._print_tag(0, element)
-            self._explore_tree(element, 1)
-            print()
+    def build_dict(self):
+        '''  loop of all the top level elements '''
 
-    @staticmethod
-    def _attribute_dict(element):
-        return dict(zip(element.attrib, map(lambda x: x.replace('xs:', ''), element.attrib.values())))
+        self.schema_dict = {}
 
-    @staticmethod
-    def _extract_tag(element):
-        return element.tag.replace('{http://www.w3.org/2001/XMLSchema}', '')
+        for main_Type in self.schema_tree.findall('*'):
+            if self._extract_tag(main_Type) == 'simpleType':
+                self.schema_dict[main_Type.get('name')] = self._simple(main_Type)
+            elif self._extract_tag(main_Type) == 'complexType':
+                self.schema_dict[main_Type.get('name')] = {}
+                for element in main_Type:
+                    self.schema_dict[main_Type.get('name')].update(self._recursion(element))
 
-    @staticmethod
-    def _extract_parent_tag(element):
-        return element.getparent().tag.replace('{http://www.w3.org/2001/XMLSchema}', '')
+    def _recursion(self, element):
 
-    def _print_tag(self, depth, element):
         element_tag = self._extract_tag(element)
-        spacer = '    '
-        if element_tag not in ['annotation', 'documentation', 'p', 'appinfo']:
-            pnt_str = f'    {spacer * depth}{element_tag}'
-            if element.items():
-                pnt_str += ' - ' + str(self._attribute_dict(element))
-            print(pnt_str)
+        current_level_dict = dict()
 
-    def _explore_tree(self, element, depth=-1):
-        '''Recusively explore the eTree'''
+        if element_tag in ['choice', 'sequence']:
+            current_level_dict[element_tag] = []
 
-        if depth < 10:
             for sub_element in element:
-                if type(sub_element) != etree._Comment:
-                    self._print_tag(depth, sub_element)
-                    self._explore_tree(sub_element, depth+1)
+                current_level_dict[element_tag].append(self._recursion(sub_element))
 
-    def _get_simple_data(self, simple_type):
+        elif element_tag in ['element', 'simpleType']:
+            current_level_dict = self.elem_functions[element_tag](element)
+
+        return current_level_dict
+
+    def _element(self, element):
+        element_attributes = self._extract_attributes(element)
+
+        for simpleType in element:
+            element_attributes.update(self._simple(simpleType))
+
+        return element_attributes
+
+    @staticmethod
+    def _simple(simple_type):
 
         simple_dict = {}
 
@@ -115,146 +117,37 @@ class SchemaTools:
 
         return simple_dict
 
+    @staticmethod
+    def _extract_attributes(element):
+        return dict(zip(element.attrib, map(lambda x: x.replace('xs:', ''), element.attrib.values())))
 
-class DifSchema(SchemaTools):
-    def __init__(self, schema_url_input=SCHEMA_URL_DIF):
-        '''
-        init
-        '''
+    @staticmethod
+    def _extract_tag(element):
+        return element.tag.replace('{http://www.w3.org/2001/XMLSchema}', '')
 
-        SchemaTools.__init__(self, schema_url_input)
-        self.build_dict()
+    ############################################
+    # printing functions for debug and testing #
+    ############################################
 
-    def build_dict(self):
-        '''
-        Creates a custom python dictionary from the XSD file selected upon init or given in self.xsd_import().
-        '''
+    def print_xsd_structure(self):
+        for count, element in enumerate(self.schema_tree.findall('*')):
+            print(f'{count}: {self._extract_tag(element)}: {element.get("name")}')
+            self._explore_tree(element)
+            print()
 
-        self.schema_dict = {}
+    def _print_tag(self, depth, element):
+        element_tag = self._extract_tag(element)
+        spacer = '    '
+        if element_tag not in ['annotation', 'documentation', 'p', 'appinfo']:
+            pnt_str = f'    {spacer * depth}{element_tag}'
+            if element.items():
+                pnt_str += ' - ' + str(self._extract_attributes(element))
+            print(pnt_str)
 
-        self.schema_dict.update(self._element_loop('simpleType', self._get_simple_data))
-        self.schema_dict.update(self._element_loop('complexType', self._get_complex_data))
+    def _explore_tree(self, element, depth=1):
 
-        return self.schema_dict
-
-    def _element_loop(self, elem_type_str, get_data_func):
-
-        loop_dict = {}
-
-        for elem_obj in self.schema_tree.findall(BASE_SCHEMA + elem_type_str):
-            elem_name = elem_obj.get('name')
-            loop_dict[elem_name] = get_data_func(elem_obj)
-
-        return loop_dict
-
-    def _get_complex_data(self, complex_type):
-
-        complex_dict = {}
-
-        # choices
-        for choice in complex_type.findall(BASE_SCHEMA + 'choice'):
-            complex_dict['sequences'] = []
-
-            # sequences?
-            for count_seq, sequence in enumerate(choice.findall(BASE_SCHEMA + 'sequence')):
-                complex_dict['sequences'].append({'elements': []})
-
-                # sequences?
-                for count_elem, element in enumerate(sequence):
-                    complex_dict['sequences'][count_seq]['elements'].append({})
-                    complex_dict['sequences'][count_seq]['elements'][count_elem].update(self._attribute_dict(element))
-
-                    # simpleTypes
-                    for simple_type in element.findall(BASE_SCHEMA + 'simpleType'):
-                        simple_dict = self._get_simple_data(simple_type)
-                        complex_dict['sequences'][count_seq]['elements'][count_elem].update(simple_dict)
-
-        return complex_dict
-
-
-class EchoSchema(SchemaTools):
-    def __init__(self, schema_url_input=SCHEMA_URL_ECHO):
-        '''init'''
-        SchemaTools.__init__(self, schema_url_input)
-
-        self.elem_functions = {'sequence': self._sequence,
-                               'element': self._element,
-                               'simpleType': self._simpleType}
-
-        self._temp = dict()
-        self._temp_sub = dict()
-        self.build_dict()
-
-    def build_dict(self):
-
-        self.schema_dict = {}
-
-        for element in self.schema_tree.findall('*'):
-            if element.tag == (BASE_SCHEMA + 'simpleType') or element.tag == (BASE_SCHEMA + 'complexType'):
-
-                element_name = element.get('name')
-
-                self._temp = {}
-                self.schema_dict[element_name] = self._explore_tree_sub(element, 1)
-
-    def _explore_tree_sub(self, element, depth=-1):
-        '''
-        each explore tree only looks at the children of one top level element
-        such as simpleType of complexType
-        cleans the temp dict and builds a new one. The sub-functions build on it, they do not return it between themselves
-        '''
-
-        if depth < 10:
+        if element.getchildren():
             for sub_element in element:
-                if self._extract_tag(sub_element) in self.elem_functions.keys():  # if there is a function for the element tag
-                    self._build_temp(sub_element)
-                    self._explore_tree_sub(sub_element, depth+1)
-
-        return self._temp
-
-    def _build_temp(self, element):
-        '''
-        runs correct function for the current element
-        '''
-
-        self.elem_functions[self._extract_tag(element)](element)
-
-    def _sequence(self, element):
-        if self._is_base(element):
-            self._temp = {'sequence': []}
-            self._temp_sub = self._temp['sequence']
-
-    def _element(self, element):
-        '''are elements always inside of sequences?'''
-
-        # parent is [sequence, root]
-        if self._extract_tag(element.getparent()) == 'sequence' and self._is_base(element.getparent()):
-            # self._temp_sub.append(element.attrib)  #
-            self._temp['sequence'].append(self._attribute_dict(element))
-
-            # if this element has a valid child, then it will prepare the temp dict for that child to store into
-            child_tags = list(map(self._extract_tag, element.getchildren()))
-            for child_tag in child_tags:
-                if child_tag in self.elem_functions.keys():
-                    pass
-                    # self._temp_sub = self._temp_sub[-1]
-
-    def _simpleType(self, element):
-
-        # this is a HORRIBLE method for this. there should be some kind of built in path I can check against in the
-        # lxml library. I might need to parse the path a little bit if it includes memory addresses
-        # this method is just a quick proof of concept.
-        if self._extract_parent_tag(element) == 'element':
-            if self._extract_parent_tag(element.getparent()) == 'sequence':
-                if self._extract_parent_tag(element.getparent().getparent()) == 'complexType':
-                    if self._extract_parent_tag(element.getparent().getparent().getparent()) == 'schema':
-
-                        # this wont work if we skipped a child to the parent when storing
-                        # finding the index of the parent element within the sequence
-                        index = element.getparent().getparent().index(element.getparent())
-                        self._temp['sequence'][index].update(self._get_simple_data(element))
-
-    def _is_base(self, element):
-        '''tests if element is direct child of a simple/complex/etc'''
-
-        return self._extract_tag(element.getparent().getparent()) == 'schema'
+                if type(sub_element) != etree._Comment:
+                    self._print_tag(depth, sub_element)
+                    self._explore_tree(sub_element, depth+1)
