9060e279dd299ee3d4330595e280a74e3cfb38e4
Carson Davis <CarsonReidDavis@gmail.com>
2019-02-25 17:13:26 -0600

    update to work with generalized schema_builder

--- unittesting.py
+++ unittesting.py
@@ -1,13 +1,16 @@
 import unittest
-import json
-from schema_builders import DifSchema
-import os
+
+from schema_builders import *
+
+SCHEMA_URL_DIF = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd'
+SCHEMA_URL_ECHO = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/echo-schemas/raw/schemas/10.0/Collection.xsd'
 
 BENCHMARK_SCHEMA_URL = 'https://git.earthdata.nasa.gov/projects/EMFD/repos/dif-schemas/raw/10.x/UmmCommon_1.3.xsd?at=refs%2Fheads%2Fmaster'
 BENCHMARK_JSON_PATH = 'test files/DIF-10_benchmark.json'
 TEST_JSON_PATH = 'test files/DIF-10_test.json'
 
-class TestDifSchemaBuilder(unittest.TestCase):
+
+class TestDIF(unittest.TestCase):
 
     def setUp(self):
         with open(BENCHMARK_JSON_PATH) as json_file:
@@ -19,11 +22,9 @@ class TestDifSchemaBuilder(unittest.TestCase):
         '''
 
         # create class object and build dict
-        test = DifSchema(BENCHMARK_SCHEMA_URL)
-        test_dict = test.build_dict()
-
+        test = SchemaBuilder(BENCHMARK_SCHEMA_URL)
         # compare the dicts
-        self.assertEqual(test_dict, self.BENCHMARK_JSON)
+        self.assertEqual(test.schema_dict, self.BENCHMARK_JSON)
 
     def test_save_json(self):
         '''
@@ -31,13 +32,12 @@ class TestDifSchemaBuilder(unittest.TestCase):
         '''
 
         # generate the test JSON
-        test = DifSchema(BENCHMARK_SCHEMA_URL)
+        test = SchemaBuilder(BENCHMARK_SCHEMA_URL)
+
         test.save_json(TEST_JSON_PATH)
 
         with open(TEST_JSON_PATH) as json_file:
             test_json = json.load(json_file)
-
-        # os.remove(TEST_JSON_PATH)
 
         # compare the JSON files
         self.assertEqual(test_json, self.BENCHMARK_JSON)
