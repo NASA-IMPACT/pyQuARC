8ab8324ef3d083da7cbea3ffab5c88a587a2e757
Ashish Acharya <ashish.acharya14@gmail.com>
2019-11-04 14:28:29 -0600

    better variable names etc

--- code/lib/xml_to_json.py
+++ code/lib/xml_to_json.py
@@ -3,17 +3,17 @@ import xmltodict
 
 from pathlib import Path
 
-p = Path(__file__).resolve().parents[2]
-
+# go up two directories # ../../
+path = Path(__file__).resolve().parents[2]
 
 INPUT_XSD = 'MetadataCommon.xsd'
 OUTPUT_JSON = 'metadata.json'
 
-INPUT_DIR = p / 'data' / 'xsd'
-OUTPUT_DIR = p / 'output'
+INPUT_DIR = path / 'data' / 'xsd'
+OUTPUT_DIR = path / 'output'
 
 with open(INPUT_DIR / INPUT_XSD, 'r') as collection_xml:
-    o = xmltodict.parse(collection_xml.read())
+    parsed = xmltodict.parse(collection_xml.read())
 
 with open(OUTPUT_DIR / OUTPUT_JSON, 'w') as metadata:
-    metadata.write(json.dumps(o))
+    metadata.write(json.dumps(parsed))
