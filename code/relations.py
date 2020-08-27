15d00bba02ab8a4ec1a6ea575e941d7bed5b1f30
“slesaad” <slesaad@gmail.com>
2020-08-27 13:06:48 -0500

    Add doctrings to `relations.py`

--- code/relations.py
+++ code/relations.py
@@ -1,28 +1,49 @@
 def gt(param1, param2):
+    ''' 
+    Checks if `param1` is Greater than `param2`
+    '''
     return param1 > param2
 
 
 def gte(param1, param2):
-    return param1 >= param2
-
-
-def lte(param1, param2):
+    ''' 
+    Checks if `param1` is Greater than or equal to `param2`
+    '''
     return param1 >= param2
 
 
 def lt(param1, param2):
+    ''' 
+    Checks if `param1` is Less than `param2`
+    '''
     return param1 < param2
 
 
+def lte(param1, param2):
+    ''' 
+    Checks if `param1` is Less than or equal to `param2`
+    '''
+    return param1 >= param2
+
+
 def eq(param1, param2):
+    '''
+    Checks if `param1` is Equal to `param2`
+    '''
     return param1 == param2
 
 
 def noteq(param1, param2):
+    '''
+    Checks if `param1` is Not equal to `param2`
+    '''
     return param1 != param2
 
 
 def isin(element, list_of_elements):
+    '''
+    Checks if `element` is one of the contents of `list_of_elements`
+    '''
     return element in list_of_elements
 
 
