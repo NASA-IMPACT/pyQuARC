2d37ea8e0533b3a2ee79797e2768190e64581540
“slesaad” <slesaad@gmail.com>
2020-10-16 13:31:41 -0500

    Move some functions from validator to utils

--- code/validator.py
+++ code/validator.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from urlextract import URLExtract
 
 from .constants import SCHEMA_PATHS
+from .utils import prepare_received_gcmd_keywords_list, prepare_gcmd_keywords_dict
 
 
 class BaseValidator:
@@ -180,32 +181,6 @@ class StringValidator(BaseValidator):
             "valid": BaseValidator.compare(input_url, vocabulary, "is_in"),
             "value": input_url
         }
-
-    @staticmethod
-    def prepare_gcmd_keywords_dict(all_keywords):
-        combined_keywords = {}
-        for row in all_keywords:
-            # converting the keywords to lowercase and
-            # stripping any whitespaces for consistency
-            keyword = '/'.join([keyword.upper().strip() for keyword in row[:-1]])
-            keyword = keyword.strip('/')
-            # making it a dict to make it more efficient to check for values
-            combined_keywords[keyword] = True
-        return combined_keywords
-
-    @staticmethod
-    def prepare_received_gcmd_keywords_list(*args):
-        keywords_lists_unordered = [arg for arg in args if arg is not None]
-        ordered_keyword_list = list(zip(*keywords_lists_unordered))
-        received_keywords = []
-        for keywords in ordered_keyword_list:
-            received_keywords.append(
-                # converting the keywords to uppercase and
-                # stripping any whitespaces for consistency
-                # stripping any extra slashes in case there's no value for the field
-                '/'.join([keyword.upper().strip() for keyword in keywords]).strip('/')
-            )
-        return received_keywords
     
     @staticmethod
     def gcmd_keywords_check(*args):
@@ -228,11 +203,11 @@ class StringValidator(BaseValidator):
         Returns:
             (dict) An object with the validity of the check and the instance
         """
-        GCMD_KEYWORDS = StringValidator.prepare_gcmd_keywords_dict(
+        GCMD_KEYWORDS = prepare_gcmd_keywords_dict(
             StringValidator.all_keywords
         )
 
-        received_keywords = StringValidator.prepare_received_gcmd_keywords_list(*args)
+        received_keywords = prepare_received_gcmd_keywords_list(*args)
 
         valid = True
         value = []
