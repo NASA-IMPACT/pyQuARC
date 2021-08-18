969c40a3c37c042180e26fb4198f83723f789614
“slesaad” <slesaad@gmail.com>
2021-08-18 16:46:16 -0500

    Add version to package

--- pyQuARC/__init__.py
+++ pyQuARC/__init__.py
@@ -1,4 +1,11 @@
 from pyQuARC.main import ARC
+from pyQuARC.version import __version__
+
 from pyQuARC.code.checker import Checker
+from pyQuARC.code.downloader import Downloader
+
+from pyQuARC.code.base_validator import BaseValidator
 from pyQuARC.code.datetime_validator import DatetimeValidator
 from pyQuARC.code.schema_validator import SchemaValidator
+from pyQuARC.code.string_validator import StringValidator
+from pyQuARC.code.url_validator import UrlValidator
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+from distutils.util import convert_path
+
+package_info = {}
+version_path = convert_path('pyQuARC/version.py')
+with open(version_path) as version_file:
+    exec(version_file.read(), package_info)
 
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
@@ -11,7 +17,7 @@ with open("requirements.txt", "r", encoding="utf-8") as requirement_file:
 
 setuptools.setup(
     name="pyQuARC",
-    version="1.0.0",
+    version=package_info['__version__'],
     author="NASA IMPACT",
     author_email="teamimpact@uah.edu",
     description="The pyQuARC tool is an open source library for Earth Observation Metadata quality and assessment. The pyQuARC tool reads and evaluates metadata records with a focus on the consistency and robustness of the metadata. pyQuARC flags opportunities to improve or add to contextual metadata information in order to help the user connect to relevant data products. pyQuARC also ensures that information common to both the data product and the file-level metadata are consistent and compatible. pyQuARC frees up human evaluators to make more sophisticated assessments such as whether an abstract accurately describes the data and provides the correct contextual information. The base pyQuARC package assesses descriptive metadata used to catalog Earth observation data products and files. As open source software, pyQuARC can be adapted and customized by data providers to allow for quality checks that evolve with their needs, including checking metadata not covered in base package.",
