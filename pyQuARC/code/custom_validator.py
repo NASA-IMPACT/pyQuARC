08cd8a61880d105f87d91cf5c1cc9d0e1f5e988a
“slesaad” <slesaad@gmail.com>
2022-02-28 10:10:54 -0600

    Refactor

--- pyQuARC/code/custom_validator.py
+++ pyQuARC/code/custom_validator.py
@@ -158,10 +158,19 @@ class CustomValidator(BaseValidator):
     def collection_progress_consistency_check(collection_state, ends_at_present_flag, ending_date_time):
         # Logic: https://github.com/NASA-IMPACT/pyQuARC/issues/61
         validity = True
-        if collection_state.upper() in ["ACTIVE", "IN WORK"]:
-            validity = (not bool(ending_date_time)) and ends_at_present_flag and ends_at_present_flag.lower() == "true"
-        elif collection_state.upper() == "COMPLETE":
-            validity = bool(ending_date_time) and (not bool(ends_at_present_flag) or (ends_at_present_flag and ends_at_present_flag.lower() == "false"))
+        collection_state = collection_state.upper()
+        ending_date_time_exists = bool(ending_date_time)
+        ends_at_present_flag_exists = bool(ends_at_present_flag)
+        ends_at_present_flag = str(ends_at_present_flag_exists) if ends_at_present_flag_exists else None
+
+        if collection_state in ["ACTIVE", "IN WORK"]:
+            validity = (not ending_date_time_exists) and (ends_at_present_flag == "true")
+        elif collection_state == "COMPLETE":
+            validity = ending_date_time_exists and (
+                not ends_at_present_flag_exists or (
+                    ends_at_present_flag == "false"
+                )
+            )
         else:
             validity = False
         return {
