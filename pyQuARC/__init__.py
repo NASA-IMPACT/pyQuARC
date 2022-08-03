ace5cdace10319c2c5659a3baac995f94548ba9f
Slesa Adhikari <slesaad@gmail.com>
2022-08-03 10:15:18 -0500

    Use constant for supported formats instead

--- pyQuARC/__init__.py
+++ pyQuARC/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from pyQuARC.main import ARC
 
-from pyQuARC.code.constants import SUPPORTED_FORMATS
+from pyQuARC.code.constants import SUPPORTED_FORMATS as FORMATS
 
 from pyQuARC.code.checker import Checker
 from pyQuARC.code.downloader import Downloader
@@ -21,20 +21,3 @@ def version():
     """Returns the current version of pyQuARC.
     """
     return __version__
-
-def supported_formats():
-    """Returns a list of metadata formats supported by pyQuARC.
-       One of the formats in the list can be passed to ARC.validate()
-       Default is `echo-c`.
-
-       "dif10": Dif10 collectioin metadata format
-       "echo-c": Echo10 collection metadata format
-       "umm-c": UMM json collection metadata format
-       "umm-g": UMM json granule metadata format
-       "echo-g": Echo10 granule metadata format
-
-    Returns:
-        list:   list of supported metadata formats
-                eg: ["dif10", "echo-c", "umm-c", "umm-g", "echo-g"]
-    """
-    return SUPPORTED_FORMATS
