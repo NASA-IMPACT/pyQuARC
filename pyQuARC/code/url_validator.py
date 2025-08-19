02d3c6e44fe0d2cf079f7897511ebfdacf6a4314
Bhawana Karakheti <bhawanakarakheti2019@gmail.com>
2025-08-19 14:46:30 -0500

    Replacing PR

--- pyQuARC/code/url_validator.py
+++ pyQuARC/code/url_validator.py
@@ -66,34 +66,126 @@ class UrlValidator(StringValidator):
 
         # check that URL returns a valid response
         for url in urls:
-            if not url.startswith("http"):
-                url = f"http://{url}"
-            try:
-                response_code = status_code_from_request(url)
-                if response_code == 200:
-                    if url.startswith("http://"):
-                        secure_url = url.replace("http://", "https://")
-                        if status_code_from_request(secure_url) == 200:
-                            result = {
-                                "url": url,
-                                "error": "The URL is secure. Please use 'https' instead of 'http'.",
-                            }
+            if url.startswith("http"):
+                try:
+                    response_code = status_code_from_request(url)  
+                    if response_code == 200:
+                        if url.startswith("http://"):
+                            secure_url = url.replace("http://", "https://")
+                            if status_code_from_request(secure_url) == 200:
+                                result = {
+                                    "url": url,
+                                    "error": f"The {url} is secure. Please use 'https' instead of 'http'.",
+                                }     
+                        else:
+                            continue
                     else:
-                        continue
-                else:
-                    result = {"url": url, "error": f"Status code {response_code}"}
-            except requests.ConnectionError:
-                result = {"url": url, "error": "The URL does not exist on Internet."}
-            except:
-                result = {"url": url, "error": "Some unknown error occurred."}
-            results.append(result)
-
+                        result = {"url": url, "error": f"Status code {response_code}"}
+                except requests.ConnectionError:
+                    result = {"url": url, "error": f"The URL {url} does not exist on Internet."}
+                except:
+                    result = {"url": url, "error": "Some unknown error occurred."}
+                results.append(result)
+            
         if results:
             validity = False
             value = results
+        
+        return {"valid": validity, "value":  value}
 
-        return {"valid": validity, "value": value}
+    @staticmethod
+    @if_arg
+    def protocol_checks(text_with_urls):
+        """
+        Checks the ftp included in `text_with_urls`
+        Args:
+           text_with_urls (str, required): The text that contains ftp
+        Returns:
+            (dict) An object with the validity of the check and the instance/results
+        """
 
+        results = []
+
+        validity = True
+
+        # extract URLs from text
+        extractor = URLExtract(cache_dir=os.environ.get("CACHE_DIR"))
+        urls = extractor.find_urls(text_with_urls)
+        urls.extend(UrlValidator._extract_http_texts(text_with_urls))
+
+        # remove dots at the end (The URLExtract library catches URLs, but sometimes appends a '.' at the end)
+        # remove duplicated urls
+        urls = set(url[:-1] if url.endswith(".") else url for url in urls)
+        value = ", ".join(urls)
+
+        # check that URL is ftp or http
+        for url in urls:
+            if url.startswith("ftp://"):
+                results.append({
+                "url": url, 
+                "error": f"The URL {url} exists"
+                })
+           
+        if results:
+            validity = False
+            value = results
+        
+        return {"valid": validity, "value":  value}
+
+    @staticmethod
+    @if_arg
+    def secure_url_checks(text_with_urls):
+        """
+        Checks the ftp included in `text_with_urls`
+        Args:
+           text_with_urls (str, required): The text that contains ftp
+        Returns:
+            (dict) An object with the validity of the check and the instance/results
+        """
+
+        results = []
+
+        validity = True
+
+        # extract URLs from text
+        extractor = URLExtract(cache_dir=os.environ.get("CACHE_DIR"))
+        urls = extractor.find_urls(text_with_urls)
+        urls.extend(UrlValidator._extract_http_texts(text_with_urls))
+
+        # remove dots at the end (The URLExtract library catches URLs, but sometimes appends a '.' at the end)
+        # remove duplicated urls
+        urls = set(url[:-1] if url.endswith(".") else url for url in urls)
+        value = ", ".join(urls)
+
+        # check that URL is ftp or http
+        for url in urls:
+            if url.startswith("http://"):
+                results.append({
+                "url": url, 
+                "error": f"The URL {url} is not secure"
+                })
+           
+        if results:
+            validity = False
+            value = results
+        
+        return {"valid": validity, "value":  value}
+
+    @staticmethod
+    @if_arg
+    def doi_check(doi):
+        """
+        Checks if the doi link given in the text is a valid doi link
+
+        Returns:
+            (dict) An object with the validity of the check and the instance/results
+        """
+        valid = False
+        if doi.strip().startswith("10."):  # doi always starts with "10."
+            url = f"https://www.doi.org/{doi}"
+            valid = UrlValidator.health_and_status_check(url).get("valid")
+        return {"valid": valid, "value": doi}
+    
     @staticmethod
     @if_arg
     def doi_check(doi):
